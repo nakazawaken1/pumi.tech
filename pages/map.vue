2015b9f77737a97902734849859f9426e5f9fadb
nakazawaken1 <nakazawaken1@gmail.com>
2021-05-27 09:22:10 +0000

    add map

---
+++ pages/map.vue
@@ -0,0 +1,32 @@
+<template>
+  <main id="map" style="height: 100%"></main>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  head() {
+    return {
+      script: [
+        {
+          src: "https://api.mapbox.com/mapbox-gl-js/v2.2.0/mapbox-gl.js",
+          defer: true,
+          body: true,
+        },
+        {
+          src: "/map.js",
+          defer: true,
+          body: true,
+        },
+      ],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://api.mapbox.com/mapbox-gl-js/v2.2.0/mapbox-gl.css",
+          body: true,
+        },
+      ],
+    };
+  },
+});
+</script>
---
+++ static/map.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibmFrYXphd2FrZW4xIiwiYSI6ImNrcDZubDlmODAxZzkydm1yZXhsN21pY3cifQ.TouoIsHyFN1XSxo2vtzuag";
+new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [135.308889,34.826806],
+  zoom: 18,
+});
