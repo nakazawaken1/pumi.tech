fefcbe74a3386c8d880291506905b34b3d857652
nakazawaken1 <nakazawaken1@users.noreply.github.com>
2021-05-30 12:41:43 +0000

    deploy: 382907ae18c8ab1baa94a54d8736662e4589af34

---
+++ _nuxt/c403b43.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{245:function(n,e,t){"use strict";t.r(e);var l=t(1).a.extend({}),c=t(44),component=Object(c.a)(l,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("main",[n._v("\n  記事12\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
--- map.js
+++ map.js
@@ -1,8 +1,10 @@
-mapboxgl.accessToken =
-  "pk.eyJ1IjoibmFrYXphd2FrZW4xIiwiYSI6ImNrcDZubDlmODAxZzkydm1yZXhsN21pY3cifQ.TouoIsHyFN1XSxo2vtzuag";
-new mapboxgl.Map({
-  container: "map",
-  style: "mapbox://styles/mapbox/streets-v11",
-  center: [135.308889,34.826806],
-  zoom: 18,
+navigator.geolocation.getCurrentPosition(function (position) {
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoibmFrYXphd2FrZW4xIiwiYSI6ImNrcDZubDlmODAxZzkydm1yZXhsN21pY3cifQ.TouoIsHyFN1XSxo2vtzuag";
+  new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: [position.coords.longitude, position.coords.latitude],
+    zoom: 18,
+  });
 });
